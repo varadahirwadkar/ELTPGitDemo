28d9e00598066a37c4ebe9de33a83870d6bc66a1
varadahirwadkar <varad.ahirwadkar368@gmail.com>
2021-12-16 09:17:23 +0530

    Update tryout.sh

--- tryout.sh
+++ tryout.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 run_script() {
+  if [ ! -d "versions" ];
+    then
+        mkdir  "versions"
+  fi
+
   for x in 6 7 8 9 10
   do
   VERSION=`curl -X GET "https://quay.io/api/v1/repository/openshift-release-dev/ocp-release/tag/?onlyActiveTags=true" | jq '.tags | sort_by(.start_ts) | .[].name'  | grep 4.$x | grep "ppc64le"  | grep -v multi | tail -1 |  tr -d '"'`
